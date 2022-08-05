6b32723bc853a3bc9bf264f310c0245542a0c3f6
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-05 10:27:01 -0700

    added virtual fields

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -5,5 +5,8 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     public int MachineId { get; set; }
     public int EngineerMachineId { get; set; }
+
+    public virtual Engineer Engineer { get; set; }
+    public virtual Machine Machine { get; set; }
   }
 }
