060a2dad8e043017f8fa3896687a2b9e256c7256
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-05 13:49:21 -0700

    updated routing

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -21,26 +21,32 @@ namespace Factory.Controllers
       return View(model);
     }
   
-  public ActionResult Details(int id)
-  {
-    var thisMachine = _db.Machines
-        .Include(machine => machine.JoinEntities)
-        .ThenInclude(join => join.Engineer)
-        .FirstOrDefault(machine => machine.MachineId == id);
-    return View(thisMachine);
-  }
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+          .Include(machine => machine.JoinEntities)
+          .ThenInclude(join => join.Engineer)
+          .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
 
-   public ActionResult Create()
+    public ActionResult Create()
     {
       return View();
     }
 
-  [HttpPost]
-  public ActionResult Create(Machine machine)
-  {
-    _db.Machines.Add(machine);
-    _db.SaveChanges();
-    return RedirectToAction("Index");
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
-}
 }
